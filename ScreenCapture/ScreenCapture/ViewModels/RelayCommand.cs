e503bdefeeca6d3141dbcb81901d3883f7a43d33
BingFengHung <joe94008@gmail.com>
2022-05-30 08:39:49 +0800

    <feat>: 加入 RelayCommand

---
+++ ScreenCapture/ScreenCapture/ViewModels/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ScreenCapture
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        public Func<bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+            else
+            {
+                return _canExecute();
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+    }
+}
