5fc1cd9da7c000287862e9822338c71f0a6f9f16
BingFengHung <joe94008@gmail.com>
2022-05-30 08:26:51 +0800

    <feat>: 加入 ShellView 與 ShellViewModel

---
+++ ScreenCapture/ScreenCapture/Views/ShellView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ScreenCapture
+{
+    /// <summary>
+    /// ShellView.xaml 的互動邏輯
+    /// </summary>
+    public partial class ShellView : UserControl
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
