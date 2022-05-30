90e0eec99b615a7d82d3c4a554d30e1d93ed27e9
BingFengHung <joe94008@gmail.com>
2022-05-30 08:35:59 +0800

    <feat>: 加入 ViewModelBase 類別

---
+++ ScreenCapture/ScreenCapture/ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ScreenCapture
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
