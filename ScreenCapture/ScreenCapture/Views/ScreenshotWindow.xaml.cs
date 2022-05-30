5f7d68b49e8370f89cafea9dc733799d6ee9aed9
BingFengHung <joe94008@gmail.com>
2022-05-30 08:48:47 +0800

    <feat>: 加入 ScreenshotWindow 與 ScreenshotViewModel

---
+++ ScreenCapture/ScreenCapture/Views/ScreenshotWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ScreenCapture
+{
+    /// <summary>
+    /// ScreenshotWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class ScreenshotWindow : Window
+    {
+        public ScreenshotWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
